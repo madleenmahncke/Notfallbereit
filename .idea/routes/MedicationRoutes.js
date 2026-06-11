3fb0edf73b3dc00e08bc0734dd2f3acab85616eb
May <madleen.mahncke@uni-oldenburg.de>
2026-06-11 15:49:11 +0200

    adding delete Medication

--- .idea/routes/MedicationRoutes.js
+++ .idea/routes/MedicationRoutes.js
@@ -5,5 +5,6 @@ const medicationController = require('../controllers/MedicationController');
 
 router.post('/:emergencyProfileId', medicationController.createMedication);
 router.put('/:emergencyProfileId/:id', medicationController.updateMedication)
+router.delete('/:emergencyProfileId/:id', medicationController.deleteMedication)
 
 module.exports = router;
